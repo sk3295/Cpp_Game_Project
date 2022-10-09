634b3274a0701051ea149d4d80a9e06c23ae8d6f
sk3295 <skltepone@gmail.com>
2022-10-09 16:45:42 +0900

    커서 이동 기능 추가

--- FoorBlueWitch/FoorBlueWitch.cpp
+++ FoorBlueWitch/FoorBlueWitch.cpp
@@ -157,6 +157,12 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow) {
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
     switch (message) {
+    case WM_LBUTTONDOWN:
+        POINT cursor;
+        
+        GetCursorPos(&cursor);
+
+        break;
     case WM_COMMAND: {
             int wmId = LOWORD(wParam);
             // 메뉴 선택을 구문 분석
